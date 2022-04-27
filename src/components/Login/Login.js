a0efb07a46eef7ee9a9a6d6e414553510f38573f
Manisho7 <yegurlamanish7@gmail.com>
2022-04-27 00:49:37 -0500

    last changes

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -18,52 +18,76 @@ const emailReducer = (state, action) => {
   return { value: "", isValid: false };
 };
 
+const passwordReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 6 };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.trim().length > 6 };
+  }
+  return { value: "", isValid: false };
+};
+
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState("");
   // const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState("");
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     val: "",
-    isValid: undefined,
+    isValid: false,
+  });
+  // console.log(emailState)
+
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    val: "",
+    isValid: false,
   });
 
-  useEffect(()=>{
-    console.log('effect running!')
-    return ()=>{
-      console.log('effect cleanup..')
-    }
-  },[]);
   // useEffect(() => {
-  //   //checks for form validation after 500ms of inactivity which stops
-  //   //browser reload less times unlike for evry key stroke
-  //   const identifier = setTimeout(() => {
-  //     console.log("timer!");
-  //     setFormIsValid(
-  //       enteredEmail.includes("@") && enteredPassword.trim().length > 6
-  //     );
-  //   }, 500);
+  //   console.log("effect running!");
   //   return () => {
-  //     //by clearing the timeout we are making sure the form validation is done exactly after 500ms.
-  //     clearTimeout(identifier);
-  //     console.log("cleanup");
+  //     console.log("effect cleanup..");
   //   };
-  // }, [enteredEmail, enteredPassword]);
+  // }, []);
+  
+  // Object destructuring by pulling out the isValid value and storing it in a new const.
+  const {isValid: emailIsValid} = emailState;
+  const {isValid: passwordIsValid} = passwordState;
+
+  useEffect(() => {
+    //checks for form validation after 500ms of inactivity which stops
+    //browser reload less times unlike for evry key stroke
+    const identifier = setTimeout(() => {
+      console.log("checking form validity!");
+      setFormIsValid(
+        emailIsValid && passwordIsValid
+      );
+    }, 500);
+    // console.log(emailState)
+    return () => {
+      //by clearing the timeout we are making sure the form validation is done exactly after 500ms.
+      clearTimeout(identifier);
+      console.log("cleanup");
+    };
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     // setEnteredEmail(event.target.value);
+    // emailState.value = 'default';
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
-
-    setFormIsValid(
-      event.target.value.includes("@") && enteredPassword.trim().length > 6
-    );
+    console.log(emailState.value)
+    // console.log(event.target.value)
+    // setFormIsValid(event.target.value.includes("@") && passwordState.isValid);
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
-    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
+    // console.log(passwordState)
+    // setEnteredPassword(event.target.value);
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
+    // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
   const validateEmailHandler = () => {
@@ -72,12 +96,13 @@ const Login = (props) => {
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    // setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({ type: "INPUT_BLUR" });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -99,14 +124,14 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ""
+            passwordState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
